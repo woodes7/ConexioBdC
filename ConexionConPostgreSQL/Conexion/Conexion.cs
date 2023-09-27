930aed7401d3f60002c34f398bcbe788b3faa6fe
DESKTOP-MAO03DS\Puesto4 <pablo112891@gmail.com>
2023-09-27 14:45:44 +0200

    w

---
+++ ConexionConPostgreSQL/Conexion/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Runtime.Remoting.Messaging;
+
+namespace ConexionConPostgreSQL.Conexion
+{
+    internal class Conexion
+    {
+        NpgsqlConnection conex = new NpgsqlConnection();
+        
+        static String servidor = "localhost";
+        static String bd = "gestorBibliotecaPersonal";
+        static String usuario = "postgres";
+        static String pasword = "";
+        static String puerto = "5432";
+
+        String CadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + "," + "pasword=" + pasword + ";" + "database=" + bd + ";" ;
+             
+
+        public NpgsqlConnection EstablecerConexion()
+        {
+            try
+            { conex.ConnectionString = CadenaConexion;
+                conex.Open();
+                MessageBox.Show("Se conecto correctamente");
+                Console.WriteLine("Se conecto correctamente");
+
+            }
+            catch (NpgsqlException e)
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos de PostgresSQL, error;" + e.ToString());
+
+            }
+            return conex;
+        }
+            
+        
+    }
+}
