9138e983501e9b4e1d47fd36f8fc2521d6a53df6
DESKTOP-MAO03DS\Puesto4 <pablo112891@gmail.com>
2023-09-28 14:45:43 +0200

    1

---
+++ ConexionConPostgreSQL/Controladores/Program.cs
@@ -0,0 +1,45 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionConPostgreSQL
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Creamos Instancia de la clase de conexión
+                Conexion.ImplementacionConexion conexion = new Conexion.ImplementacionConexion();
+
+                //Establecer la conexión a la base de datos
+                NpgsqlConnection dbConnection = conexion.EstablecerConexion();
+                
+                if (dbConnection != null && dbConnection.State == System.Data.ConnectionState.Open)
+                {                  
+                    string query = "SELECT * FROM gbp_almacen.gbp_alm_cat_libros ORDER BY id_libro ASC";
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, dbConnection);
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+                    
+                    // Mostrar
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["columna1"] + " " + reader["columna2"]+ reader["columna3"] + reader["columna4"] + reader["columna5"] );
+                    }
+
+                    // Cerramos
+                    dbConnection.Close();
+                }
+                else
+                {
+                    Console.WriteLine("No se pudo establecer la conexión a la base de datos.");
+                }
+
+                Console.ReadLine(); 
+            }
+        }
+}
+
---
+++ ConexionConPostgreSQL/Servicios/ImplementacionConexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Runtime.Remoting.Messaging;
+using ConexionConPostgreSQL.Servicios;
+using System.Configuration;
+
+namespace ConexionConPostgreSQL.Conexion
+{
+    internal class ImplementacionConexion : InterfazConexion
+    {
+        public NpgsqlConnection InterfazConexionEestableceConexion()
+        {
+           
+
+        NpgsqlConnection InterfazConexion.EstableceConexion()
+        {
+                //Se lee la cadena de conexión a Postgresql del archivo de configuración
+                string stringConexionPostgresql = ConfigurationManager.ConnectionStrings["stringConexion"].ConnectionString;
+                Console.WriteLine("[INFORMACIÓN-ConexionPostgresqlImplementacion-generarConexionPostgresql] Cadena conexión: " + stringConexionPostgresql);
+
+                NpgsqlConnection conexion = null;
+                string estado = "";
+
+                if (!string.IsNullOrWhiteSpace(stringConexionPostgresql))
+                {
+                    try
+                    {
+                        conexion = new NpgsqlConnection(stringConexionPostgresql);
+                        conexion.Open();
+                        //Se obtiene el estado de conexión para informarlo por consola
+                        estado = conexion.State.ToString();
+                        if (estado.Equals("Open"))
+                        {
+
+                            Console.WriteLine("[INFORMACIÓN-ConexionPostgresqlImplementacion-generarConexionPostgresql] Estado conexión: " + estado);
+
+                        }
+                        else
+                        {
+                            conexion = null;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("[ERROR-ConexionPostgresqlImplementacion-generarConexionPostgresql] Error al generar la conexión:" + e);
+                        conexion = null;
+                        return conexion;
+
+                    }
+                }
+
+                return conexion;
+            }
+        }
+    }
+}
